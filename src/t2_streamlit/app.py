017b3bf503f19e64816bafea0e982b22ac9ff9ba
j25ng <jsan2861@gmail.com>
2024-10-07 14:52:06 +0900

    add camera

--- src/t2_streamlit/app.py
+++ src/t2_streamlit/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-page_1 = st.Page("pages/upload_pic.py", title="Upload Picture", icon="🖼️")
-
-pg = st.navigation([page_1])
+page_1 = st.Page("pages/upload.py", title="Upload Picture", icon="🖼️")
+page_2 = st.Page("pages/camera.py", title="Take a photo", icon="📸")
+pg = st.navigation([page_1, page_2])
 pg.run()
