5c534e929c7d4437019d1988e105cdf95211a8a6
j25ng <jsan2861@gmail.com>
2024-10-10 03:40:50 +0900

    진짜 최종

--- src/t2_streamlit/pages/tmp/chart.backup.py
+++ src/t2_streamlit/pages/tmp/chart.backup.py
@@ -22,11 +22,18 @@ st.title("Chart")
 def load_data(path):
     ip = os.getenv("EC2_IP", "localhost")
     url = f"http://{ip}:8022/{path}"
+
     r = requests.get(url).json()
     return r
 
 data_agg = load_data("agg")
 df = pd.DataFrame(data_agg)
+
+df['accuracy'] = df['correct'] / df['total_count']
+df['age_band'] = df['answer'].map(age_band_mapping)
+df = df.sort_values(by="answer")
+
+chart_data = df[['age_band', 'accuracy']].set_index('age_band')
 
 total_correct = df['correct'].sum()
 total_incorrect = df['total_count'].sum() - total_correct
@@ -36,20 +43,19 @@ sizes = [total_correct, total_incorrect]
 explode = (0.1, 0)
 
 fig, ax = plt.subplots(figsize=(4, 4))
-ax.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-        shadow=True, startangle=90)
+ax.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
 ax.axis('equal')
+
+data_all = load_data("all")
+df = pd.DataFrame(data_all).dropna(subset=['prediction_result', 'answer'])
+df['error'] = df['prediction_result'].astype(int) - df['answer'].astype(int)
+
+error_counts = df['error'].value_counts().reindex(range(0, 9), fill_value=0)
+error_counts = error_counts[error_counts > 0]
 
 with st.container():
     st.subheader("전체 데이터 예측 정확도 통계", divider="gray")
     st.pyplot(fig)
-
-age_band_order = list(age_band_mapping.values())
-df['accuracy'] = df['correct'] / df['total_count']
-df['age_band'] = df['answer'].map(age_band_mapping)
-df['age_band'] = pd.Categorical(df['age_band'], categories=age_band_order, ordered=True)
-df = df.sort_values(by="age_band")
-chart_data = df[['age_band', 'accuracy']].set_index('age_band')
 
 with st.container():
     st.subheader("나이대별 예측 정확도 통계", divider="gray")
@@ -58,13 +64,6 @@ with st.container():
         x_label="Age Band",
         y_label="Prediction Accuracy"
     )
-
-data_all = load_data("all")
-df_a = pd.DataFrame(data_all)
-
-df_a['error'] = abs(df_a['prediction_result'].astype(int) - df_a['answer'].astype(int))
-error_counts = df_a['error'].value_counts().reindex(range(0, 9), fill_value=0)
-error_counts = error_counts[error_counts > 0]
 
 with st.container():
     st.subheader("오차율 분석 통계", divider="gray")
